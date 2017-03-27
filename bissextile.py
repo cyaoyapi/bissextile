47874b26b0027f24e50191462de5682a48894c5c
cyaoyapi <cyaoyapi@gmail.com>
2017-03-27 12:35:44 +0000

    Gestion des erreurs de saisie par les exceptions

--- bissextile.py
+++ bissextile.py
@@ -8,13 +8,18 @@ print("Ce programme vous permet de tester si une année est bissexitle ou pas.\n
 quitter = raw_input("Voulez-vous demarrez le programme ?\nTapez 'o' pour 'oui' et 'n' pour 'non'\n")
 
 while quitter.upper() == "O":
-	annee = int(raw_input("Entrez l'année : \n"))
-
-	if (annee%400 == 0) or (annee%4 == 0 and annee%100 != 0):
-		print annee," est une année bissextile.\n"
+	try:
+		annee = int(raw_input("Entrez l'année : \n"))
+		if annee < 0:
+			raise ValueError("L'année que vous que vous avez saisie est négative.\n")
+	except ValueError:
+		print "L'année que vous avez saisie est soit négative ou est une alphanumérique.\n"
 	else:
-		print annee," n'est pas une année bissextile.\n"
-
-	quitter = raw_input("Voulez-vous continuez le programme ?\nTapez 'o' pour 'oui' et 'n' pour 'non'\n")
+		if (annee%400 == 0) or (annee%4 == 0 and annee%100 != 0):
+			print annee," est une année bissextile.\n"
+		else:
+			print annee," n'est pas une année bissextile.\n"
+	finally:
+		quitter = raw_input("Voulez-vous continuez le programme ?\nTapez 'o' pour 'oui' et 'n' pour 'non'\n")
 
 print("Fin du programme. Merci et à bientôt!\n")
