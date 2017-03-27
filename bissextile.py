7da8f3151bdee76467babe53faa305db83e402b5
cyaoyapi <cyaoyapi@gmail.com>
2017-03-27 11:42:43 +0000

    Optisation du code de tester en 2 lignes

--- bissextile.py
+++ bissextile.py
@@ -9,14 +9,11 @@ quitter = raw_input("Voulez-vous demarrez le programme ?\nTapez 'o' pour 'oui' e
 
 while quitter.upper() == "O":
 	annee = int(raw_input("Entrez l'année : \n"))
-	if annee%400 == 0:
-		print annee," est une année bissextile.\n"
-	elif annee%100 == 0:
-		print annee," est une année bissextile.\n"
-	elif annee%4 == 0:
+
+	if (annee%400 == 0) or (annee%4 == 0 and annee%100 != 0):
 		print annee," est une année bissextile.\n"
 	else:
-			print annee," n'est pas une année bissextile.\n"
+		print annee," n'est pas une année bissextile.\n"
 
 	quitter = raw_input("Voulez-vous continuez le programme ?\nTapez 'o' pour 'oui' et 'n' pour 'non'\n")
 
